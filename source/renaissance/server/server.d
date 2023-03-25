21f170a9993f1e864ca0cd1c4889b70b6a951807
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-25 15:37:01 +0200

    Server
    - Added type `Server` which represents an instance of the daemon

---
+++ source/renaissance/server/server.d
@@ -0,0 +1,11 @@
+module renaissance.server.server;
+
+/** 
+ * Represents an instance of the daemon which manages
+ * all listeners attached to it, server state and
+ * message processing
+ */
+public class Server
+{
+    
+}
