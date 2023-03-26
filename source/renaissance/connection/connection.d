3107f0a400604309cd6227c48fa4f542747254ac
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-26 12:04:32 +0200

    Connection
    - The `Connection` type is now a kind-of `Thread`
    - Added field `associatedServer` of type `Server` such that a connection can be associated with a given server
    - Added field `clientSocket` such that a connection can associate the socket with itself
    - Constructor sets the two above fields and calls the `Thread` super constructor passing in a function pointer to the worker function (currently a while loop)
    - Implemented `Connection newConnection(Server, Socket)` which produces a new `Connection`, then calls `Server.addConnection()` and finally starts the `Connection`'s thread via `start()`, then returns the Connection

--- source/renaissance/connection/connection.d
+++ source/renaissance/connection/connection.d
@@ -1,6 +1,59 @@
 module renaissance.connection.connection;
 
-public class Connection
+import davinci;
+import core.thread : Thread;
+import std.socket : Socket;
+import renaissance.server;
+
+public class Connection : Thread
 {
-    
+    private Server associatedServer;
+    private Socket clientSocket;
+
+    private this(Server associatedServer, Socket clientSocket)
+    {
+        this.associatedServer = associatedServer;
+        this.clientSocket = clientSocket;
+
+        /* Set the worker function for the thread */
+        super(&worker);
+    }
+
+    private void worker()
+    {
+        // TODO: Add worker function here
+        while(true)
+        {
+            // TODO: Addn a tasky/tristanable queue managing thing with
+            // ... socket here (probably just the latter)
+            // ... which decodes using the `davinci` library
+        }
+    }
+
+    /** 
+     * Creates a new connection by associating a newly created
+     * Connection instance with the provided Server and Socket
+     * after which it will be added to the server's connection
+     * queue, finally starting the thread that manages this connection
+     *
+     * Params:
+     *   associatedServer = the server to associate with
+     *   clientSocket = the associated socket backing the client
+     *
+     * Returns: the newly created Connection object
+     */
+    public static Connection newConnection(Server associatedServer, Socket clientSocket)
+    {
+        Connection conn = new Connection(associatedServer, clientSocket);
+
+        /* Associate this connection with the provided server */
+        associatedServer.addConnection(conn);
+
+        /* Start the worker on a seperate thread */
+        // new Thread(&worker);
+        conn.start();
+
+
+        return conn;
+    }
 }
